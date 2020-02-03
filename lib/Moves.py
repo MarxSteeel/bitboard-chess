6db319a25cd0a71cb15a58bf2a173f2fb5df7682
MarxSteel <kurcoalt@gmail.com>
2020-02-03 15:38:54 -0300

    Implemented possible moves in all directions for rooks, bishops and queen

--- lib/Moves.py
+++ lib/Moves.py
@@ -2,6 +2,10 @@ def trailing_zeros(s):
     return len(s) - len(s.rstrip("0"))
 
 
+def reverse(n):
+    return int('{:064b}'.format(n)[::-1], 2)
+
+
 class Moves(object):
     def __init__(self, board):
         self.utilities = {
@@ -23,11 +27,26 @@ class Moves(object):
                 board.pieces["WQ"] | board.pieces["WK"] | board.pieces["WP"] | board.pieces["BK"]),
             "black_pieces": (board.pieces["BR"] | board.pieces["BN"] |
                 board.pieces["BB"] | board.pieces["BQ"] | board.pieces["BP"]),
+            "occupied": (board.pieces["WR"] | board.pieces["WN"] | board.pieces["WB"] | 
+                board.pieces["WQ"] | board.pieces["WK"] | board.pieces["WP"] | 
+                board.pieces["BR"] | board.pieces["BN"] | board.pieces["BB"] |
+                board.pieces["BQ"] | board.pieces["BK"] | board.pieces["BP"]),
             "empty": ~(board.pieces["WR"] | board.pieces["WN"] | board.pieces["WB"] | 
                 board.pieces["WQ"] | board.pieces["WK"] | board.pieces["WP"] | 
                 board.pieces["BR"] | board.pieces["BN"] | board.pieces["BB"] |
-                board.pieces["BQ"] | board.pieces["BK"] | board.pieces["BP"]), 
-            "pieces": board.pieces
+                board.pieces["BQ"] | board.pieces["BK"] | board.pieces["BP"]),
+            "pieces": board.pieces, 
+            "rank_masks": [0xFF, 0xFF00, 0xFF0000, 0xFF000000, 0xFF00000000, 0xFF0000000000, 0xFF000000000000, 0xFF00000000000000],
+            "file_masks": [0x101010101010101, 0x202020202020202, 0x404040404040404, 0x808080808080808,
+                0x1010101010101010, 0x2020202020202020, 0x4040404040404040, 0x8080808080808080],
+            "diagonal_masks": [0x1, 0x102, 0x10204, 0x1020408, 0x102040810, 0x10204081020,
+                0x1020408102040, 0x102040810204080, 0x204081020408000, 0x408102040800000,
+                0x810204080000000, 0x1020408000000000, 0x2040800000000000, 0x4080000000000000,
+                0x8000000000000000],  # Top left to bottom right
+            "anti_diagonal_masks": [0x80, 0x8040, 0x804020, 0x80402010, 0x8040201008,
+                0x804020100804, 0x80402010080402, 0x8040201008040201, 0x4020100804020100,
+                0x2010080402010000, 0x1008040201000000, 0x804020100000000, 0x402010000000000,
+                0x201000000000000, 0x100000000000000]  # Top right to bottom left
         }
 
     def white_pawn_moves(self, pawn, history=None):
@@ -100,3 +119,19 @@ class Moves(object):
                     moves_list += str(i%8 + 1) + str(i%8) + "E"
         return moves_list
 
+    def hv_moves(self, spot):
+        all = self.utilities
+        binary_s = 1 << spot
+        horizontal_moves = (all["occupied"] - 2*binary_s) ^ (reverse(reverse(all["occupied"]) - 2 * reverse(binary_s)))
+        vertical_moves = ((all["occupied"] & all["file_masks"][spot % 8]) - (2*binary_s)) ^ \
+            (reverse(reverse(all["occupied"] & all["file_masks"][spot % 8]) - 2 * reverse(binary_s)))
+        return bin((horizontal_moves & all["rank_masks"][spot//8]) | (vertical_moves & all["file_masks"][spot%8]))
+
+    def diag_moves(self, spot):
+        all = self.utilities
+        binary_s = 1 << spot
+        diagonal_moves = ((all["occupied"] & all["diagonal_masks"][spot // 8 + spot % 8]) - (2*binary_s)) ^ \
+            (reverse(reverse(all["occupied"] & all["diagonal_masks"][spot // 8 + spot % 8]) - 2 * reverse(binary_s)))
+        anti_diagonal_moves = ((all["occupied"] & all["anti_diagonal_masks"][(spot // 8 + 7) - spot % 8]) - (2*binary_s)) ^ \
+            (reverse(reverse(all["occupied"] & all["diagonal_masks"][(spot // 8 + 7) - spot % 8]) - 2 * reverse(binary_s)))
+        return bin((diagonal_moves & all["diagonal_masks"][spot//8 + spot%8]) | (anti_diagonal_moves & all["anti_diagonal_masks"][(spot//8 + 7) - spot%8]))
--- main.py
+++ main.py
@@ -5,10 +5,13 @@ board = BoardGeneration()
 pieces = board.pieces
 # print(pieces)
 # print(bin(pieces["WR"]))
-print(board.chessBoard)
-print(board.pieces)
-print(board.drawArray())
+# print(board.chessBoard)
+# print(board.pieces)
+# print(board.drawArray())
 moves = Moves(board)
 history = [["BN", "0120"], ["BP", "1636"]]
+# print(len(bin(moves.utilities["rank_masks"][4])))
 # print(moves.utilities)
 print(moves.white_pawn_moves(pieces["WP"], history))
+print(moves.hv_moves(35))  # Por algun motivo no funciona con numeros < 8
+print(moves.diag_moves(35))
